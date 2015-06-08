a2af43728b410b5eea35975e8cf08aa36ab83931
ArturLoska <artur.loska@windowslive.com>
2015-06-09 00:24:41 +0200

    Adding jquery to reflinks, making addictional partiar view with controller

---
+++ ArturLoskaSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ArturLoskaSite.Startup))]
+namespace ArturLoskaSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ ArturLoskaSite/SurfaceControllers/PartialSurfaceController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArturLoskaSite.SurfaceControllers
+{
+    public class PartialSurfaceController : SurfaceControler
+
+    {
+        // GET: PartialSurface
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
